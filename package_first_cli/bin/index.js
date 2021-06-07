3df697851512675890f327d478b5fe2324bd81c1
xiahong <807649737@qq.com>
2021-06-07 11:34:52 +0800

    fix eventloop

--- package_first_cli/bin/index.js
+++ package_first_cli/bin/index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 const lib = require('package_first_lib');
-console.log(lib.sum(1,2));
-console.log(lib.mul(2,2));
+// console.log(lib.sum(1,2));
+// console.log(lib.mul(2,2));
+console.log(lib.handler());
 
-
-console.log('welcome package_first_cli!')
+// console.log('welcome package_first_cli!')
