2b1b729be4b42e3e99b3986593e79b0498023584
xiahong <807649737@qq.com>
2021-06-16 15:49:54 +0800

    fix core

--- package_demo_cli/core/core/bin/index.js
+++ package_demo_cli/core/core/bin/index.js
@@ -7,7 +7,7 @@
 const importLocal = require('import-local');
 
 if(importLocal(__filename)){
-    require('npmlog').info('cli','正在使用 package_demo_cli 本地版本')
+    require('npmlog').info('core','正在使用 package_demo_cli 本地版本')
 } else {
     require('../lib/core')(process.argv.slice(2))
 }
