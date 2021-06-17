630aabc07ba504098fa02d14be20cd7da5609102
xiahong <807649737@qq.com>
2021-06-17 16:48:33 +0800

    fix nodeVersion

--- package_demo_cli/core/core/lib/index.js
+++ package_demo_cli/core/core/lib/index.js
@@ -3,22 +3,28 @@
 module.exports = core;
 //外部依赖在前面
 const semver = require('semver');
+const colors = require('colors/safe');
+
 // require支持.js .json ,其他有js代码的文件
 const pkg = require('../package.json');
 const log = require('@package_demo_cli/log');
 const constant = require('./version');
 
 function core() {
-    checkPkgVersion();
-    checkNodeVersion();
+    try {
+        checkPkgVersion();
+        checkNodeVersion();
+    } catch (e) {
+        log.error(e.message);
+    }
 }
 
 function checkPkgVersion() {
-    log.say('test','go');
-    log.success('haha','success...');
-    log.verbose('debug','debug...');
-    log.notice('core',pkg.version);
-    log.info('core',pkg.name);
+    log.say('test', 'go');
+    log.success('haha', 'success...');
+    log.verbose('debug', 'debug...');
+    log.notice('core', pkg.version);
+    log.info('core', pkg.name);
 }
 
 function checkNodeVersion() {
@@ -27,6 +33,6 @@ function checkNodeVersion() {
     // 第二步,对比最低版本号
     const lowestVersion = constant.LOWEST_NODE_VERSION;
     if (!semver.gte(currentVersion, lowestVersion)) {
-        throw new Error(colors.red(`package_demo_cli 需要安装 v${lowest} 及以上版本的 Node.js`));
+        throw new Error(colors.red(`package_demo_cli 需要安装 v${lowestVersion} 及以上版本的 Node.js`));
     }
 }
--- package_demo_cli/core/core/lib/version.js
+++ package_demo_cli/core/core/lib/version.js
@@ -1,4 +1,4 @@
-const LOWEST_NODE_VERSION = '12.0.0';
+const LOWEST_NODE_VERSION = '15.0.0';
 
 module.exports = {
     LOWEST_NODE_VERSION
