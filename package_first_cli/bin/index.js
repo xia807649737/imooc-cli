76812a8680176be8e9e4761b81dcbc2127e4f349
xiahong <807649737@qq.com>
2021-06-22 18:39:31 +0800

    fix commander

--- package_first_cli/bin/index.js
+++ package_first_cli/bin/index.js
@@ -1,1 +1,13 @@
 #!/usr/bin/env node
+
+const commander = require('commander');
+
+const pkg = require('../package.json');
+
+//获取commander的单例
+// const { program } = commander;
+const program = new commander.Command();
+
+program
+    .version(pkg.version)
+    .parse(process.argv);
