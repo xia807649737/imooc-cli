c2af4eba0aa57355189995d1c56a3642d76751c4
xiahong <807649737@qq.com>
2021-06-23 18:40:27 +0800

    fix service

--- package_first_cli/bin/index.js
+++ package_first_cli/bin/index.js
@@ -1,7 +1,7 @@
 #!/sr/bin/env node
 
 const commander = require('commander');
-const { option, describe } = require('yargs');
+const { option, describe, command } = require('yargs');
 
 const pkg = require('../package.json');
 
@@ -29,7 +29,26 @@ clone
     .option('-f, --force', '是否强制克隆')
     .action((source, destination, cmdObj) => {
         console.log('do clone', source, destination, cmdObj.force);
-    })
+    });
+
+//addCommand 注册子命令
+const service = new commander.Command('service');
+service
+    .command('start [port]')
+    .description('start service at some port')
+    .action(port => {
+        console.log('do service start', port)
+    });
+service
+    .command('stop')
+    .description('stop service')
+    .action(() => { 
+        console.log('please stop service')
+    });
+
+
+program.addCommand(service);
+
 
 program
     .parse(process.argv);
