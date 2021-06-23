9992a70e92ad120f85a74718dfe02dd8e46d68ef
xiahong <807649737@qq.com>
2021-06-23 16:34:43 +0800

    fix command

--- package_first_cli/bin/index.js
+++ package_first_cli/bin/index.js
@@ -1,6 +1,7 @@
-#!/usr/bin/env node
+#!/sr/bin/env node
 
 const commander = require('commander');
+const { option } = require('yargs');
 
 const pkg = require('../package.json');
 
@@ -9,5 +10,14 @@ const pkg = require('../package.json');
 const program = new commander.Command();
 
 program
+    .name(Object.keys(pkg.bin)[0])
+    .usage('<command> [options]')
     .version(pkg.version)
+    .option('-d, --debug', '是否开启调试模式', false)
+    .option('-e, --envName <envName>', '获取环境变量名称')
     .parse(process.argv);
+
+    // console.log(program._optionValues.debug); 
+    // console.log(program._optionValues.envName);    
+    // program.outputHelp();
+    // console.log(program.opts());
