44ffcfbff396779b059c5925bb272edc5f12fcac
xiahong <807649737@qq.com>
2021-06-29 17:27:54 +0800

    fix execute

--- package_demo_cli/core/exec/lib/index.js
+++ package_demo_cli/core/exec/lib/index.js
@@ -3,12 +3,26 @@
 const Package = require('@package_demo_cli/package');
 const log = require('@package_demo_cli/log');
 
+const SETTINGS = {
+    init:'@package_demo_cli/init',
+}
+
 const exec = () => {
     const targetPath = process.env.CLI_TARGET_PATH;
     const homePath = process.env.CLI_HOME_PATH;
-    log.verbose('targetPath', targetPath)
-    log.verbose('homePath', homePath)
-    const pkg = new Package();
+    log.verbose('targetPath', targetPath);
+    log.verbose('homePath', homePath);
+
+    const cmdObj = process.argv[process.argv.length - 5];
+    // const cmdObj = arguments[arguments.length - 1];
+    const packageName = SETTINGS[cmdObj];
+    const packageVersion = 'lastest';
+    const pkg = new Package({
+        targetPath,
+        // storePath,
+        packageName,
+        packageVersion
+      });
     console.log(pkg);
 
 }
--- package_demo_cli/models/package/lib/index.js
+++ package_demo_cli/models/package/lib/index.js
@@ -1,18 +1,22 @@
 'use strict';
 
+const { isObject } = require('@package_demo_cli/utils');
 class Package {
     constructor(options) {
         if (!options) {
             throw new Error('Package类的options参数不能为空!')
+        }
+        if (!isObject(options)) {
+            throw new Error('Package类的options参数必须为对象!')
         }
         // package的路径
         this.targetPath = options.targetPath;
         // package的存储路径
         this.storePath = options.storePath;
         // package的name
-        this.packageName = options.name;
+        this.packageName = options.packageName;
         // package的version
-        this.packageVersion = options.version;
+        this.packageVersion = options.packageVersion;
     }
 
     // 判断Package是否存在
