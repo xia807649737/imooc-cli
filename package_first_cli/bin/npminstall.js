f2378a42cb2f1ae3cd53b02812f3f1b5f53e7f60
xiahong <807649737@qq.com>
2021-07-02 17:29:00 +0800

    fix npmintsall

--- package_first_cli/bin/npminstall.js
+++ package_first_cli/bin/npminstall.js
@@ -1,10 +1,11 @@
 const npminstall = require('npminstall');
 const path = require('path');
 const userHome = require('user-home');
+// console.log('userHome',userHome);
 
 npminstall({
-    root: path.resolve(userHome, '.package-demo-cli'),
-    storeDir: path.resolve(userHome, '.package-demo-cli', 'node_modules'),
+    root: path.resolve(userHome, '.package_demo_cli'),
+    storeDir: path.resolve(userHome, '.package_demo_cli', 'node_modules'),
     registry: "https://registry.npmjs.org",
     pkgs: [
         { name: 'foo', version: '~1.0.0'},
