b1af653440c735d4551eaf14ea4ec8f93ab4db3d
xiahong <807649737@qq.com>
2021-07-02 16:52:06 +0800

    fix 利用npminstall库安装npm模块

--- package_first_cli/bin/npminstall.js
+++ package_first_cli/bin/npminstall.js
@@ -3,10 +3,10 @@ const path = require('path');
 const userHome = require('user-home');
 
 npminstall({
-    root: path.resolve(userHome, './package_fist_cli'),
-    storeDir: path.resolve(userHome, './package_fist_cli', 'node_modules'),
+    root: path.resolve(userHome, '.package-demo-cli'),
+    storeDir: path.resolve(userHome, '.package-demo-cli', 'node_modules'),
     registry: "https://registry.npmjs.org",
     pkgs: [
-        { name: 'foo', version: '^~1.0.0'},
+        { name: 'foo', version: '~1.0.0'},
     ],
 })
