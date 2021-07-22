592f03137dc5aa1b094c13af8c3263c1612f09e0
xiahong <807649737@qq.com>
2021-07-22 16:10:33 +0800

    fix spwan的用法

--- package_first_cli/bin/process/index.js
+++ package_first_cli/bin/process/index.js
@@ -2,8 +2,8 @@ const cp = require('child_process');
 const path = require('path');
 // console.log(cp);
 
-const child = cp.spawn(path.resolve(__dirname, 'test.shell'), ['dir'], {
-    cwd: path.resolve('..')
+const child = cp.spawn(process.platform === 'win32' ? 'cnpm.cmd':'cnpm', ['install'], {
+    cwd: path.resolve('/Users/v-hongxia.abcft/Desktop/imooc-cli/package_first_lib')
 })
 
 child.stdout.on('data', function(chunk){
