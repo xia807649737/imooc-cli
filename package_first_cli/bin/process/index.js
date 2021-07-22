766cc73e24bb45c55b82c852b767203e95241a07
xiahong <807649737@qq.com>
2021-07-22 15:30:58 +0800

    fix spwan

---
+++ package_first_cli/bin/process/index.js
@@ -0,0 +1,15 @@
+const cp = require('child_process');
+const path = require('path');
+// console.log(cp);
+
+const child = cp.spawn(path.resolve(__dirname, 'test.shell'), ['dir'], {
+    cwd: path.resolve('..')
+})
+
+child.stdout.on('data', function(chunk){
+    console.log('stdout', chunk.toString());
+})
+
+child.stderr.on('data', function (chunk) { 
+    console.log('stderr', chunk.toString())
+})
