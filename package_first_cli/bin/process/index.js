ab6c5b05c75db9e9bb5fbaf93787b6693a56556a
xiahong <807649737@qq.com>
2021-07-22 17:05:24 +0800

    fix child_process spawn用法以及与exec&execFile的区别

--- package_first_cli/bin/process/index.js
+++ package_first_cli/bin/process/index.js
@@ -2,6 +2,7 @@ const cp = require('child_process');
 const path = require('path');
 // console.log(cp);
 
+//spwan:耗时任务(比如:npm install),需要不断更新日志
 const child = cp.spawn(process.platform === 'win32' ? 'cnpm.cmd':'cnpm', ['install'], {
     cwd: path.resolve('/Users/v-hongxia.abcft/Desktop/imooc-cli/package_first_lib')
 })
@@ -12,4 +13,13 @@ child.stdout.on('data', function(chunk){
 
 child.stderr.on('data', function (chunk) { 
     console.log('stderr', chunk.toString());
+})
+
+//exec/execFile:开销较小的任务
+cp.exec('cnpm install',{
+    cwd: path.resolve('/Users/v-hongxia.abcft/Desktop/imooc-cli/package_first_lib'),
+}, function (err, stdout, stderr) { 
+    console.log(err);
+    console.log(stdout);
+    console.log(stderr);
 })
