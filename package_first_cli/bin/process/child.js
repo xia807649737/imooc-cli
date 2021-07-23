edd0788428205c486a3b024038671a9cdc2b468a
xiahong <807649737@qq.com>
2021-07-23 10:58:42 +0800

    fix child_process 父子进程通信机制

--- package_first_cli/bin/process/child.js
+++ package_first_cli/bin/process/child.js
@@ -1,6 +1,10 @@
-console.log('test fork');
+console.log('child process');
+
 console.log('child pid:', process.pid);
 
+// 子进程中监听message事件
 process.on('message', msg => { 
     console.log(msg);
 })
+// 子进程向主进程中发送信息
+process.send('hello main process!');
--- package_first_cli/bin/process/index.js
+++ package_first_cli/bin/process/index.js
@@ -2,7 +2,7 @@ const cp = require('child_process');
 const path = require('path');
 // console.log(cp);
 
-//spwan:耗时任务(比如:npm install),需要不断更新日志
+//spwan: 耗时任务(比如:npm install),需要不断更新日志
 // const child = cp.spawn(process.platform === 'win32' ? 'cnpm.cmd':'cnpm', ['install'], {
 //     cwd: path.resolve('/Users/v-hongxia.abcft/Desktop/imooc-cli/package_first_lib')
 // })
@@ -15,7 +15,7 @@ const path = require('path');
 //     console.log('stderr', chunk.toString());
 // })
 
-//exec/execFile:开销较小的任务
+//exec/execFile: 开销较小的任务
 // cp.exec('cnpm install',{
 //     cwd: path.resolve('/Users/v-hongxia.abcft/Desktop/imooc-cli/package_first_lib'),
 // }, function (err, stdout, stderr) { 
@@ -28,4 +28,12 @@ const path = require('path');
 const child = cp.fork(path.resolve(__dirname, 'child.js'));
 console.log('main pid:', process.pid);
 
-child.send('hello child process!');
+// 主进程向子进程中发送消息
+child.send('hello child process!', () => { 
+    // 通信后可断开进程,通信结束,否则2遍出现等待
+    // child.disconnect();
+});
+// 主进程接收子进程的信息
+child.on('message', msg => { 
+    console.log(msg);
+})
