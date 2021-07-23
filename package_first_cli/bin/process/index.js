ef2875c1715350cd70af4115ab9b74ba0e7b933b
xiahong <807649737@qq.com>
2021-07-23 18:25:55 +0800

    fix

--- package_first_cli/bin/process/index.js
+++ package_first_cli/bin/process/index.js
@@ -2,24 +2,32 @@ const cp = require('child_process');
 const path = require('path');
 // console.log(cp);
 
+// cp.exec('dir', function (err, stdout, stderr) {
+//     console.log(err);
+//     console.log(stdout);
+//     console.log(stderr);
+// })
+
+
+
 //spwan: 耗时任务(比如:npm install),需要不断更新日志
-const child = cp.spawn(process.platform === 'win32' ? 'cnpm.cmd':'cnpm', ['install'], {
-    cwd: path.resolve('/Users/v-hongxia.abcft/Desktop/imooc-cli/package_first_lib')
-})
+// const child = cp.spawn(process.platform === 'win32' ? 'cnpm.cmd':'cnpm', ['install'], {
+//     cwd: path.resolve('/Users/v-hongxia.abcft/Desktop/imooc-cli/package_first_lib')
+// })
 
-child.stdout.on('data', function(chunk){
-    console.log('stdout', chunk.toString());
-})
+// child.stdout.on('data', function(chunk){
+//     console.log('stdout', chunk.toString());
+// })
 
-child.stderr.on('data', function (chunk) { 
-    console.log('stderr', chunk.toString());
-})
+// child.stderr.on('data', function (chunk) { 
+//     console.log('stderr', chunk.toString());
+// })
 
 //exec/execFile: 开销较小的任务
-cp.exec('cnpm install',{
-    cwd: path.resolve('/Users/v-hongxia.abcft/Desktop/imooc-cli/package_first_lib'),
-}, function (err, stdout, stderr) { 
-    console.log(err);
-    console.log(stdout);
-    console.log(stderr);
-})
+// cp.exec('cnpm install',{
+//     cwd: path.resolve('/Users/v-hongxia.abcft/Desktop/imooc-cli/package_first_lib'),
+// }, function (err, stdout, stderr) { 
+//     console.log(err);
+//     console.log(stdout);
+//     console.log(stderr);
+// })
